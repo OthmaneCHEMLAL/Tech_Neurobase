5e415805d1b7e69d38ac5cc4960dbead0c16652c
OthmaneCHEMLAL <otmanechemlal@gmail.com>
2025-01-30 16:53:26 +0100

    First commit for Tech_Neurobase

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,14 @@ use App\Http\Controllers\Auth\RegisterController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', function () {
+    return view('dashboard'); // Assure-toi d'avoir une vue 'dashboard.blade.php'
+})->middleware('auth')->name('dashboard');
